b1b94f980fd0b947349f2f63bc0e9fee8374985c
Artem Alekseevgit <artem_1285@mail.ru>
2022-08-29 20:55:22 +0500

    Random

---
+++ Example003_SUM/Program.cs
@@ -0,0 +1,10 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int numberA = 31;
+        int numberB = 5;
+        int result = numberA + numberB;
+        Console.WriteLine(result);
+    }
+}
